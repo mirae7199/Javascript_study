1b36e2b6707b95ab732815c8eb766bb149cdc2b9
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-02-06 20:30:00 +0900

    feat: type

---
+++ section01/chapter05.js
@@ -0,0 +1,33 @@
+// 1. Nubmer Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity; // 양의 무한대
+let mInf = -Infinity; // 음의 무한대
+
+let nan = NaN;
+
+console.log(1 * "hello");
+
+// 2. String Type
+let myName = "김미래";
+let myLocation = "광주"
+console.log(myName + myLocation);
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다.`
+console.log(introduceText);
+
+// 템플릿 리터럴 문법이다.
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다.)
+let empty = null; // 지금 어떠한 값도 없다.
+
+// 5. Undefined Type
+let none;
+console.log(none); // undefined이라는 값은 이 변수를 미처 초기화하지
+// 못했거나 아니면 존재하지 않는 어떤 값을 불러오려고 할 때 발생
